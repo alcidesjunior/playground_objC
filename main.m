b23a1615da7a8ab0c91d6b78f28f32c73f1e54ab
Alcides Junior <alcidesjunior.infor@gmail.com>
2018-02-16 21:34:06 -0300

    brincando com variaveis e console

--- main.m
+++ main.m
@@ -3,7 +3,11 @@
 int main (int argc, const char * argv[])
 {
         NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        int idade = 22;// = "Alcides Junior";
+        NSString* nome = @"Alcides Junior";
         NSLog (@"Hello, World!");
+        NSLog (@"Minha idade e %d",idade);
+        NSLog (@"Meu nome é %@",nome);
         [pool drain];
         return 0;
 }
